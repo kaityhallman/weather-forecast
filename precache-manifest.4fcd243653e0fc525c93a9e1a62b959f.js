41e79bead7b6bca4f8b8b8f7e6e19ccba385a9eb
Kaity Hallman <khallman@zipcar.com>
2019-07-22 09:45:13 -0400

    Updates

---
+++ precache-manifest.4fcd243653e0fc525c93a9e1a62b959f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a2b2d909e570464fd6c898e66d55af",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "a0bcb239764799d8c6fa",
+    "url": "/weather-forecast/static/css/main.1541737f.chunk.css"
+  },
+  {
+    "revision": "576955fd745658c1171e",
+    "url": "/weather-forecast/static/js/2.75b6bf08.chunk.js"
+  },
+  {
+    "revision": "a0bcb239764799d8c6fa",
+    "url": "/weather-forecast/static/js/main.666dad8e.chunk.js"
+  },
+  {
+    "revision": "c753dce0acc1d73f011b",
+    "url": "/weather-forecast/static/js/runtime~main.ff5e48c9.js"
+  },
+  {
+    "revision": "c23221d967aebaf27009e8c65a84cb6a",
+    "url": "/weather-forecast/static/media/cold.c23221d9.jpg"
+  },
+  {
+    "revision": "977f9518d27de17b5cc4e6e2ce814a7f",
+    "url": "/weather-forecast/static/media/hot.977f9518.jpg"
+  },
+  {
+    "revision": "4360d9daac521d8ce19e6ae1fdc8beba",
+    "url": "/weather-forecast/static/media/moderate.4360d9da.jpg"
+  }
+]);
